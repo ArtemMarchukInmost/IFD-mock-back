953468f55894377a7064fa889d3ffb03ed6afdd6
ArtemyMarchuk <artemmarchukwork@gmail.com>
2021-08-15 15:06:32 +0300

    user info endpoint

--- app/mock-data/userInfo.helper.js
+++ app/mock-data/userInfo.helper.js
@@ -3,7 +3,7 @@ const casual = require('casual');
 const type = 'userInfo';
 
 casual.define(type, function () {
-    const image = 'https://cs8.pikabu.ru/post_img/big/2016/02/04/7/145458292112119207.jpg';
+    const image = 'https://sunmag.me/wp-content/uploads/2016/05/gipnoz1.jpg';
     const background = 'https://img.strana.ua/img/article/2625/70_main.jpeg';
     const progress = casual.integer(0, 100);
     const workshopsCompleted = casual.integer(0, 6);
