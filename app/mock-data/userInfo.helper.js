1176e2462de119dcae626b9e522f6ecb469516b1
ArtemyMarchuk <artemmarchukwork@gmail.com>
2021-08-15 15:15:38 +0300

    user info endpoint

--- app/mock-data/userInfo.helper.js
+++ app/mock-data/userInfo.helper.js
@@ -5,7 +5,7 @@ const type = 'userInfo';
 casual.define(type, function () {
     const image = 'https://sunmag.me/wp-content/uploads/2016/05/gipnoz1.jpg';
     const background = 'https://img.strana.ua/img/article/2625/70_main.jpeg';
-    const progress = casual.integer(0, 100);
+    const progress = casual.integer(50, 100);
     const workshopsCompleted = casual.integer(0, 6);
 
     return {
